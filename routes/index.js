bced6f443c41306955b2b52a41a7719372419447
ZAMP005 <guilherme.quirino@arvato.com>
2021-11-22 01:13:06 +0200

    creation of presentation api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+var axios = require('axios');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+router.get('/:fileId', async function(req, res, next) {
+  let file
+  let offset = 0
+  let flag = true  
+  while(flag){
+    let {data} = await axios.get(`http://interview-api.snackable.ai/api/file/all?limit=5&offset=${offset}`)
+    file = data.filter(f => f.fileId === req.params.fileId)
+    if (data.length === 0 || !!file) {
+      flag = false
+    }
+    offset = offset + 5
+  }
+  res.json(file)
+
+})
+
+module.exports = router ;
